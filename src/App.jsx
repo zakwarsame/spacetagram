db3b4822fa0ad831f7e37160cff2ba6aa83eaecf
zak <zakwarsamee@gmail.com>
2022-01-09 13:06:52 -0500

    modify app.jsx so that the data is loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+lpers/apiCall";
+import PhotoList from "./components/PhotoList";
+import Container from "@mui/material/Container";
+import Navbar from "./components/Navbar";
+import { Typography } from "@mui/material";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  const [apod, setApod] = useState({});
+
+  useEffect(() => {
+    apiCall.getApod().then((apodData) => {
+      setApod(apodData.data);
+    });
+  }, []);
+  
+
+  return (
+    <>
+      <Navbar />
+      <Container maxWidth="sm">
+        <Typography variant="h4" component="div" gutterBottom>
+          Nasa Api
+        </Typography>
+        <Typography variant="h5" gutterBottom component="div">
+          Astronomy Picture of the Day
+        </Typography>
+        <PhotoList apod={apod} />
+      </Container>
+    </>
+  );
 };
 
 export default App;
