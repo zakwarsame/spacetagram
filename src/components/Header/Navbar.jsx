39eab6ebf0353db414ad5626383568a56c8002d4
zak <zakwarsamee@gmail.com>
2022-01-09 23:04:03 -0500

    set up a dark mode theme

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -4,12 +4,17 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import Switch from "@mui/material/Switch";
 import { Button } from "@mui/material";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import { ColorModeContext } from "../../providers/ThemeProvider";
 
-export default function Navbar(colorMode) {
+export default function Navbar() {
+  const colorMode = React.useContext(ColorModeContext);
+  const theme = useTheme();
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -26,9 +31,17 @@ export default function Navbar(colorMode) {
         >
           Spacetagram
         </Typography>
-        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
+        <IconButton
+          sx={{ ml: 1 }}
+          onClick={colorMode.toggleColorMode}
+          color="inherit"
+        >
+          {theme.palette.mode === "dark" ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
---
+++ src/providers/ThemeProvider.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+export const ColorModeContext = React.createContext({
+  toggleColorMode: () => {},
+});
+
+export default function ToggleColorMode(props) {
+  const [mode, setMode] = React.useState("dark");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
