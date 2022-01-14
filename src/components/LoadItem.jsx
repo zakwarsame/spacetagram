1af5b1189f815b56c42a294643a26f0bb723590b
alchemy <zakwarsame2018@gmail.com>
2022-01-14 13:21:35 -0500

    add keys to load skeleton

--- src/components/LoadItem.jsx
+++ src/components/LoadItem.jsx
@@ -7,7 +7,10 @@ export default function LoadItem({ length }) {
   return (
     <>
       {[...Array(length)].map((item, i) => (
-        <Card key={i} sx={{ padding: "20px", margin: "20px", borderRadius: "10px" }}>
+        <Card
+          key={i}
+          sx={{ padding: "20px", margin: "20px", borderRadius: "10px" }}
+        >
           <Stack spacing={1}>
             <Skeleton variant="text" />
             <Skeleton variant="text" width="30%" />
